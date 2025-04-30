18f767b8db5def1a88b0e4686c77395c0b971414
OWO Devs <developers.owo@gmail.com>
2025-04-30 12:06:27 +0200

    chore: call owo initialize on game load

--- OWO_BladeAndSorcery.cs
+++ OWO_BladeAndSorcery.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OWO_BaldeAndSorcery;
+using System;
 using ThunderRoad;
 
 
@@ -6,9 +7,12 @@ namespace OWO_BladeAndSorcery
 {
     public class OWO_BladeAndSorcery : ThunderScript
     {
+        public static OWOSkin owoSkin;
+
         public override void ScriptLoaded(ModManager.ModData modData)
         {
             base.ScriptLoaded(modData); //Load on PlayGame button pressed
+            owoSkin = new OWOSkin();
         }
     }
 }
